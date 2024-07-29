c80c90f03d8c456d6d7264587f5b1bc5371d6f1e
Jonathan <jonathan.wolf@showplacehq.com>
2024-07-28 20:52:32 -0500

    add zone?

--- sst.config.ts
+++ sst.config.ts
@@ -1,19 +1,22 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "gwdesigns-donna-violin",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: { cloudflare: true },
     };
   },
   async run() {
     new sst.aws.Nextjs("MyWeb", {
       domain: {
         name: "donnawolfviolin.com",
-        dns: sst.cloudflare.dns()
-      }
+        dns: sst.cloudflare.dns({
+          zone: "e1b0c5c873e318fce554e0bb40a68102",
+        }),
+      },
+      
     });
   },
 });
